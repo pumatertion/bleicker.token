4e99f02bfaadcbab3aa56a15c78fc2d6a121721d
Carsten Bleicker <carsten@bleicker.de>
2015-04-25 16:50:43 +0200

    Adjust Tests

---
+++ tests/Unit/Fixtures/NoCredentialsToken.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Bleicker\Token\Unit\Fixtures;
+
+use Bleicker\Token\AbstractToken;
+
+/**
+ * Class NoCredentialsToken
+ *
+ * @package Tests\Bleicker\Token\Unit\Fixtures
+ */
+class NoCredentialsToken extends AbstractToken {
+
+	/**
+	 * @return $this
+	 */
+	public function authenticate() {
+
+		if ($this->getStatus() === self::AUTHENTICATION_SUCCESS || $this->getStatus() !== self::AUTHENTICATION_REQUIRED) {
+			return $this;
+		}
+
+		if ($this->getCredentials() === 'foo') {
+			$this->status = self::AUTHENTICATION_SUCCESS;
+			return $this;
+		}
+
+		$this->status = self::AUTHENTICATION_FAILED;
+
+		return $this;
+	}
+
+	/**
+	 * @return $this
+	 */
+	public function injectCredentialsAndSetStatus() {
+		$this->status = self::AUTHENTICATION_NOCREDENTIALSGIVEN;
+		return $this;
+	}
+}
--- tests/Unit/TokenTest.php
+++ tests/Unit/TokenTest.php
@@ -2,6 +2,11 @@
 
 namespace Tests\Bleicker\Token\Unit;
 
+use Bleicker\Token\TokenInterface;
+use Tests\Bleicker\Token\Unit\Fixtures\FailingToken;
+use Tests\Bleicker\Token\Unit\Fixtures\NoCredentialsToken;
+use Tests\Bleicker\Token\Unit\Fixtures\SuccessSessionToken;
+use Tests\Bleicker\Token\Unit\Fixtures\SuccessToken;
 use Tests\Bleicker\Token\UnitTestCase;
 
 /**
@@ -14,7 +19,56 @@ class TokenTest extends UnitTestCase {
 	/**
 	 * @test
 	 */
-	public function getCredentialsTest() {
-		$this->assertTrue(TRUE);
+	public function noCredentialsTokenTest() {
+		$token = new NoCredentialsToken();
+		$this->assertEquals(TokenInterface::AUTHENTICATION_NOCREDENTIALSGIVEN, $token->getStatus(), 'No Credentials');
+
+		$token->injectCredentialsAndSetStatus();
+		$this->assertEquals(TokenInterface::AUTHENTICATION_NOCREDENTIALSGIVEN, $token->getStatus(), 'No Credentials injected');
+
+		$token->authenticate();
+		$this->assertEquals(TokenInterface::AUTHENTICATION_NOCREDENTIALSGIVEN, $token->getStatus(), 'No Credentials after authentication');
+	}
+
+	/**
+	 * @test
+	 */
+	public function failingTokenTest() {
+		$token = new FailingToken();
+		$this->assertEquals(TokenInterface::AUTHENTICATION_NOCREDENTIALSGIVEN, $token->getStatus(), 'No Credentials');
+
+		$token->injectCredentialsAndSetStatus();
+		$this->assertEquals(TokenInterface::AUTHENTICATION_REQUIRED, $token->getStatus(), 'Credentials injected and Auth required');
+
+		$token->authenticate();
+		$this->assertEquals(TokenInterface::AUTHENTICATION_FAILED, $token->getStatus(), 'Authentication failed with injected credentials');
+	}
+
+	/**
+	 * @test
+	 */
+	public function successTokenTest() {
+		$token = new SuccessToken();
+		$this->assertEquals(TokenInterface::AUTHENTICATION_NOCREDENTIALSGIVEN, $token->getStatus(), 'No Credentials');
+
+		$token->injectCredentialsAndSetStatus();
+		$this->assertEquals(TokenInterface::AUTHENTICATION_REQUIRED, $token->getStatus(), 'Credentials injected and Auth required');
+
+		$token->authenticate();
+		$this->assertEquals(TokenInterface::AUTHENTICATION_SUCCESS, $token->getStatus(), 'Authentication failed with injected credentials');
+	}
+
+	/**
+	 * @test
+	 */
+	public function successSessionTokenTest() {
+		$token = new SuccessSessionToken();
+		$this->assertEquals(TokenInterface::AUTHENTICATION_NOCREDENTIALSGIVEN, $token->getStatus(), 'No Credentials');
+
+		$token->injectCredentialsAndSetStatus();
+		$this->assertEquals(TokenInterface::AUTHENTICATION_REQUIRED, $token->getStatus(), 'Credentials injected and Auth required');
+
+		$token->authenticate();
+		$this->assertEquals(TokenInterface::AUTHENTICATION_SUCCESS, $token->getStatus(), 'Authentication failed with injected credentials');
 	}
 }
