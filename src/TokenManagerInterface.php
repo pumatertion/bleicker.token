9e0b1e9048c7679b5d4209414da744e50fc3044b
Carsten Bleicker <carsten@bleicker.de>
2015-04-26 15:41:02 +0200

    Refactor TokenManagerInterface

--- src/TokenManagerInterface.php
+++ src/TokenManagerInterface.php
@@ -27,7 +27,37 @@ interface TokenManagerInterface {
 	public static function hasToken($alias);
 
 	/**
+	 * @param array $tokens
+	 * @return static
+	 */
+	public static function setSessionTokens(array $tokens = array());
+
+	/**
+	 * @param $alias
+	 * @return PrototypeTokenInterface|NULL
+	 */
+	public static function getSessionToken($alias);
+
+	/**
 	 * @return array
 	 */
 	public static function getPrototypeTokens();
+
+	/**
+	 * @return array
+	 */
+	public static function getSessionTokens();
+
+	/**
+	 * @param string $alias
+	 * @return boolean
+	 */
+	public static function hasSessionToken($alias);
+
+	/**
+	 * @param string $alias
+	 * @param SessionTokenInterface $token
+	 * @return static
+	 */
+	public static function registerSessionToken($alias, SessionTokenInterface $token);
 }
