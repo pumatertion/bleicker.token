c6bade8d32ce2a08ddcd5368551d5e1e1b6cc11f
Carsten Bleicker <carsten@bleicker.de>
2015-05-10 14:01:28 +0200

    Introduce Tokens Container

---
+++ src/TokensInterface.php
@@ -0,0 +1,48 @@
+<?php
+namespace Bleicker\Token;
+
+use Bleicker\Container\Exception\AliasAlreadyExistsException;
+
+/**
+ * Class Tokens
+ *
+ * @package Bleicker\Token
+ */
+interface TokensInterface {
+
+	/**
+	 * @return array
+	 */
+	public static function storage();
+
+	/**
+	 * @param string $alias
+	 * @return boolean
+	 */
+	public static function has($alias);
+
+	/**
+	 * @param string $alias
+	 * @param mixed $data
+	 * @return static
+	 * @throws AliasAlreadyExistsException
+	 */
+	public static function add($alias, $data);
+
+	/**
+	 * @return static
+	 */
+	public static function prune();
+
+	/**
+	 * @param string $alias
+	 * @return mixed
+	 */
+	public static function get($alias);
+
+	/**
+	 * @param string $alias
+	 * @return static
+	 */
+	public static function remove($alias);
+}
